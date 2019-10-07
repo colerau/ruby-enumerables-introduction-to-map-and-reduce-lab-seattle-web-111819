524930f152377c9f1d5651352e9ac9784508e2e8
Cole Rau <colevrau@gmail.com>
2019-10-07 20:26:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,7 +57,7 @@ def reduce_to_all_true(source_array)
     if source_array[i] == true 
       count += 1
     else source_array[i] == false
-      count - 1
+      count -= 1
     end
     i = i + 1
   end 
