09c74e92a190fac1f82faaa4e028dd6d06dd9394
Cole Rau <colevrau@gmail.com>
2019-10-07 20:45:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -70,7 +70,7 @@ def reduce_to_any_true(source_array)
   i = 0 
   count = 0 
   while i < source_array.length do 
-    if source_array[i]
+    if source_array[i] == true 
       return true 
     else
       return false
