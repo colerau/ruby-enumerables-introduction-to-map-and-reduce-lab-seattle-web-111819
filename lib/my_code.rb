c40bfbf2fb33515f7275a229e90641b0eead3144
Cole Rau <colevrau@gmail.com>
2019-10-07 19:56:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,21 +51,7 @@ def reduce_to_total(source_array, starting_point = 0)
 end 
 
 def reduce_to_all_true(source_array)
-  i = 0 
-  a = []
-  while i < source_array.length do 
-    if i = true
-      i += 1
-      if i == source_array.length = 1
-        if i == true 
-          return true
-        end
-      end 
-    else
-      return false
-    end
-  end
-end 
+  
   
 
   
